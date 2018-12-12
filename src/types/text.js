09804b1cce786d3ca70133f8aa1cc1a9f52ec66c
Adam Bertrand <adambertrand@Adams-MBP.attlocal.net>
2018-12-11 22:27:31 -0500

    cleans up code

--- src/types/text.js
+++ src/types/text.js
@@ -1,7 +1,6 @@
 import {setLabel} from '../common/utils';
 
 export function handleTextLogic(el, value) {
-    console.log(/^[a-zA-Z]+$/.test(value))
     if (/^[a-zA-Z]+$/.test(value)) {
         el.value = value;
         el.errors = '';
